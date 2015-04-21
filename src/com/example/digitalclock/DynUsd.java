abdf70efcf96924179104a36af16fa2c2dcae229
star003 <x00502@gmail.com>
2015-04-21 13:36:55 +0300

    2015-04-21 изменения
    запрос объемов с ММВБ перевел на финам

--- src/com/example/digitalclock/DynUsd.java
+++ src/com/example/digitalclock/DynUsd.java
@@ -27,7 +27,9 @@ public class DynUsd extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 	            WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
-		List<Integer> fldId = Arrays.asList(R.id.usLast,R.id.usOpen,R.id.usVal,R.id.usTime);
+		List<Integer> fldId = Arrays.asList(R.id.usLast,R.id.usLastTrend
+											,R.id.usOpen,R.id.usVal
+											,R.id.usTime);
 		for (int i=0;i<fldId.size();i++){
 			TextView fff = (TextView)findViewById(fldId.get(i));
 			_fields.add(fff);
@@ -37,33 +39,37 @@ public class DynUsd extends Activity {
     	mt.execute();
     		
 		Thread t = new Thread() {
-	      	  @Override
-	      	  public void run() {
-	      	    try {
-	      	      while (!isInterrupted()) {
-	      	        Thread.sleep(2000);
-	      	        runOnUiThread(new Runnable() {
-	      	          @Override
-	      	          public void run() {
-	      	        	mt = new goMCEX();
-  	      	    		mt.execute();
-	      	          }
-	      	        });
-	      	      }
+			@Override
+			public void run() {
+				try {
+					while (!isInterrupted()) {
+						Thread.sleep(2000);
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								mt = new goMCEX();
+								mt.execute();
+							}
+						});
+	      	      	}
 	      	    } catch (InterruptedException e) {
 	      	    }
-	      	  }
-	      	};
-	      	t.start();
+			}
+		};
+		t.start();
 	      	
-		}
+	}//protected void onCreate(Bundle savedInstanceState)
 
+	/////////////////////////////////////////////////////////////////////////////////////
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.dyn_usd, menu);
 		return true;
-	}
-
+	}//public boolean onCreateOptionsMenu(Menu menu)
+	
+	/////////////////////////////////////////////////////////////////////////////////////
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
@@ -71,15 +77,16 @@ public class DynUsd extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
+	}//public boolean onOptionsItemSelected(MenuItem item)
 	
 	class goMCEX extends AsyncTask<Void, Void, Void> {
 		ArrayList<String> _stringData = new ArrayList<String>();
+		
 		/////////////////////////////////////////////////////////////////////////////////////
 		
 		@Override
 	    protected void onPreExecute() {
-	      super.onPreExecute();
+			super.onPreExecute();
 	    }//protected void onPreExecute()
 
 		/////////////////////////////////////////////////////////////////////////////////////
@@ -87,9 +94,9 @@ public class DynUsd extends Activity {
 		@Override
 	    protected Void doInBackground(Void... params) {
 	    	try {
-	    		_stringData = priceBRENT.getUsdDay();
+	    		_stringData = priceBRENT.usdFinam();
 	    	} catch (IOException e) {
-	    		Log.i("m","error read priceBRENT.getUsdDay()");
+	    		Log.i("m","error read priceBRENT.usdFinam()");
 			}
 	    	return null;
 	    }//protected Void doInBackground(Void... params)
@@ -99,11 +106,13 @@ public class DynUsd extends Activity {
 		@Override
 	    protected void onPostExecute(Void result) {
 			super.onPostExecute(result);
-			if (_stringData.size()==4) {
-				for (int i=0;i<4;i++){
-					_fields.get(i).setText(_stringData.get(i));
+			List<Integer> ind = Arrays.asList(0,1,4,6,7);
+			if (_stringData.size()==7) {
+				for (int i=0;i<ind.size();i++){
+					_fields.get(i).setText(_stringData.get(ind.get(i)));
 				}
 			}	
 		}//protected void onPostExecute(Void result)
-	}
-}
+		
+	}//class goMCEX extends AsyncTask<Void, Void, Void>
+}//public class DynUsd extends Activity
